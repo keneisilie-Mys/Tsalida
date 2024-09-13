e2eb2f6470abfbad498494a13661ce22aee856f7
Kenei Sile <146013715+Keneisilie@users.noreply.github.com>
2024-09-13 23:06:49 +0530

    Updated SongFragment toolbar Changes icon when the favorites icon is clicked Updates the database accordingly

--- app/src/main/java/com/example/tsalida/SongFragment.java
+++ app/src/main/java/com/example/tsalida/SongFragment.java
@@ -1,5 +1,9 @@
 package com.example.tsalida;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,13 +13,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.util.Log;
+import android.view.ContentInfo;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -32,6 +37,8 @@ public class SongFragment extends Fragment {
     }
 
     private View rootView;
+    private Menu menuSong; //A menu reference because i want to access its icons
+    private MenuItem menuItemSong;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,22 +58,68 @@ public class SongFragment extends Fragment {
         //Changing the toolbar title in the main activity
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         activity.getSupportActionBar().setTitle("Hymn");
+        ViewPager2 viewPager2 = rootView.findViewById(R.id.imageViewPager);
         //Changing the toolbar menu of the main activity
         MenuProvider menuProvider = new MenuProvider() {
             @Override
             public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+
                 menuInflater.inflate(R.menu.song_toolbar_menu, menu);
+                MenuItem menuItem = menu.findItem(R.id.item1);
+                menuItemSong = menu.findItem(R.id.item1);
+                if(checkFavorite(position)){
+                    menuItem.setIcon(R.drawable.favorite_filled_icon);
+                }
+                ////////////////
+                //The following lines updates the menu according to the view pager
+                //For some reason setting up listener here inside the onCreateMenu works instead of outside
+                viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        super.onPageSelected(position);
+                        if(checkFavorite(position)){
+                            menuItemSong.setIcon(R.drawable.favorite_filled_icon);
+                        }else{
+                            menuItemSong.setIcon(R.drawable.favorites_icon);
+                        }
+                    }
+                });
             }
 
             @Override
             public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.item1){
+
+                    //Changing menu item icon and updating database
+                    try(SQLiteOpenHelper sqLiteOpenHelper = new FavoriteDatabase(getContext());
+                        SQLiteDatabase database = sqLiteOpenHelper.getReadableDatabase();){
+                        int position = viewPager2.getCurrentItem();
+                        Cursor cursor = database.query("FAVORITES", new String[]{"_id, IS_FAVORITE"}, "_id = ?", new String[]{Integer.toString(position+1)}, null, null, null);
+                        int fav_int = 0;
+                        if(cursor.moveToFirst()){
+                            fav_int = cursor.getInt(1);
+                        }
+                        if(fav_int == 1){
+                            menuItem.setIcon(R.drawable.favorites_icon);
+                            ContentValues contentValues = new ContentValues();
+                            contentValues.put("IS_FAVORITE", 0);
+                            database.update("FAVORITES", contentValues, "_id = ?", new String[]{Integer.toString(position+1)});
+                        }else{
+                            menuItem.setIcon(R.drawable.favorite_filled_icon);
+                            ContentValues contentValues = new ContentValues();
+                            contentValues.put("IS_FAVORITE", 1);
+                            database.update("FAVORITES", contentValues, "_id = ?", new String[]{Integer.toString(position+1)});
+                        }
+                        database.close();
+                        cursor.close();
+                    }
+                }
+
                 return false;
             }
         };
         requireActivity().addMenuProvider(menuProvider, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
 
-        ////////////////////////////////////////
-        ViewPager2 viewPager2 = rootView.findViewById(R.id.imageViewPager);
 
         //Getting the ids of the images with their names to an array
         int[] imageIds = new int[396];
@@ -81,8 +134,29 @@ public class SongFragment extends Fragment {
         viewPager2.setPageTransformer(new DepthPageTransformer());
         viewPager2.setCurrentItem(position, false);
 
+
+
+
         // Inflate the layout for this fragment
         return rootView;
+    }
+
+    private boolean checkFavorite(int position) {
+        try{
+            SQLiteOpenHelper sqLiteOpenHelper = new FavoriteDatabase(getContext());
+            SQLiteDatabase database = sqLiteOpenHelper.getReadableDatabase();
+            Cursor cursor = database.query("FAVORITES", new String[]{"_id, IS_FAVORITE"}, "_id = ?", new String[]{Integer.toString(position+1)}, null, null, null);
+            if(cursor.moveToFirst()){
+                if(cursor.getInt(1) == 1){
+                    return true;
+                }
+            }
+            database.close();
+            cursor.close();
+        }catch (Exception e){
+            Log.d("Error", "Error");
+        }
+        return false;
     }
 
 }
